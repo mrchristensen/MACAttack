b41e8a358a9eae8f5089e21a5f4a6033b65bf757
Matt <mr.christensen@gmail.com>
2021-09-24 11:44:25 -0600

    Removed unused function

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 import sha1
-import struct
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(sha1.sha1(bytes("GeeksForGeeks", encoding='utf8')))
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
